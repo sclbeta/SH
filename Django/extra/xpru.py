deefad10456dffbaefd51d3959364544860a8192
shaochanglong1000@163.com <huanxiong1000>
2015-01-08 19:25:15 +0800

    改变信息传输方式，添加fifo程序，Django不再直接调用串口程序；添加命令解析程序及相应文件

--- Django/extra/xpru.py
+++ Django/extra/xpru.py
@@ -1,10 +1,23 @@
 import os
-from time import sleep
 
 if not os.path.exists('/tmp/mklkfifo'):
     os.system('mkfifo /tmp/mklkfifo')
 
-def fass(mklk):
+def duqu(wfjmmk,ww):
+    temp = {}
+    f = file(wfjmmk,'r')
+    alllines = f.readlines()
+    for eachline in alllines:
+        x,y = eachline.split(':')
+        temp[x] = y
+    f.close()
+    return temp[ww]
+
+
+def fass(wwvsdrhc,wwmklk):
+    x = duqu('vsdrhc',wwvsdrhc)
+    y = duqu('mklk',wwmklk)
+    mklk = x + y
     f = file('mklkfifo','w')
-    f.write(str(x))
+    f.write(mklk)
     f.close()
