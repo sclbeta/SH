de86243136154bbb540628dd1177607ea56026c0
sclbeta <vvaa00@126.com>
2015-01-10 15:47:10 +0800

    将xpru文件添加到extra文件

--- Django/light/views.py
+++ Django/light/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404
 from light.models import Light
-
-from extra1 import xpru
-
+from extra.xpru import fass
 #from django.contrib.auth.decorators import login_required
-
 #@login_required(login_url="/login/")
 def index(request):
     jpgids = [True,True]
     for i in range(1,3):
         l = get_object_or_404(Light,pk = i)
         id= l.status
-
         if id:
             jpgids[i-1] = True
         else:
@@ -27,12 +23,10 @@ def detail(request,pk):
     pk = int(pk)
     if st:
         st = True
-        xpru.fass(gp[pk-1])
         l.status = 0
         l.save()
     else:
         st = False
-        xpru.fass(gp[pk+1])
         l.status = 1
         l.save()
 
--- Django/manage.py
+++ Django/manage.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
+	if not os.path.exists('/tmp/mklkfifo'):
+		os.system('mkfifo /tmp/mklkfifo')
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
